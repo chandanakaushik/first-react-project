3e28f58751930a7311816abe944b806a13880d7d
Likith Sirsikar <likiths3@gmail.com>
2025-03-30 12:28:11 +0530

    feat(props): props and jsx

--- my-react-app/src/App.jsx
+++ my-react-app/src/App.jsx
@@ -1,19 +1,35 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
-import HelloWorld, {HelloWorld1} from './HelloWorld'
+import { useState } from 'react';
+import reactLogo from './assets/react.svg';
+import viteLogo from '/vite.svg';
+import './App.css';
+import HelloWorld, { HelloWorld1 } from './HelloWorld';
 
 function App() {
-  const [count, setCount] = useState(0)
+  const abc = [1, 2, 3, 4].map((i) => i * 1);
+  console.log('abc', abc);
+
+  const handleClickHelloWorld = () => {
+    console.log('Hello World is clicked');
+  };
+
+  const one = 2;
+
+  if (one === 1) {
+    return (
+      <>
+        <h1>The value is one</h1>
+      </>
+    );
+  }
 
   return (
-    <> 
-        <HelloWorld />
-        <HelloWorld1 />
-      
+    <>
+      <button onClick={handleClickHelloWorld}>Click</button>
+      {[1, 2, 3, 4, 5, 6, 7, 8, 9, 10].map((i) => (
+        <HelloWorld i={i} text={'Chandana'} />
+      ))}
     </>
-  )
+  );
 }
 
-export default App
+export default App;
--- my-react-app/src/HelloWorld.jsx
+++ my-react-app/src/HelloWorld.jsx
@@ -1,26 +1,42 @@
-function HelloWorld() {
-    return (
-        <>
-        <div>
-            <h1>Hello world!</h1>
-        </div>
-        </>
-    )
+import React from 'react';
+
+function HelloWorld({ text, i }) {
+  return (
+    <>
+      <div>
+        <h1>
+          Hello {text} {i}
+        </h1>
+      </div>
+    </>
+  );
 }
 
-export default HelloWorld 
+// class ComponaName extends React.Component {
+//   render() {
+//     return (
+//       <>
+//         <div id="container" className="12345">
+//           <h1>Hello world! abc</h1>
+//         </div>
+//       </>
+//     );
+//   }
+// }
 
+// export default ComponaName;
+export default HelloWorld;
 //Default export
 
-export const  HelloWorld1 = () => {
-    return (
-        <>
-        <div>
-            <h1>Hello world!</h1>
-        </div>
-        </>
-    )
-}
-
+export const HelloWorld1 = () => {
+  const abc = '12356';
+  return (
+    <>
+      <div id={abc} className="12345">
+        <h1>Hello world!</h1>
+      </div>
+    </>
+  );
+};
 
 //Named export
